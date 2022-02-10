9efe5108ac1653e0f71fd6f4a5e1275520da0cce
Divino Junio Batista Lopes <junio.batista12@gmail.com>
2022-02-10 18:34:28 -0300

    Redirecionador completo exceto pelo append para a saida, executa comandos em primeiro plano, falta versao

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,32 @@
 #include "../include/Tabela.h"
+#include <fcntl.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
 #define MAX_SIZE_PATH 100
 
 vector<vector<string>> historico;
+Tabela tabela = Tabela(".unbshrc");
+
+vector <string> split(string linha);
+void printVetor(vector<char *> tokens);
+void printVetor(vector<string> tokens);
+int executa(vector<string> linhaComando, string inArquivo, string outArquivo);
+void trataBuiltin(vector<string> linhaComando, string inArquivo, string outArquivo);
+void parse(vector<string> tokens);
+
+int main(){
+    tabela.printTabela();
+    string entrada;
+    while (entrada != "exit"){
+        char cwd[100];
+        getcwd(cwd,sizeof(cwd));
+        cout << "UnBsh-user1-" << cwd<< ">";
+        getline(cin, entrada);
+        vector<string> tokens = split(entrada);
+        parse(tokens);
+    }
+}
 
 vector <string> split(string linha){
     vector<string> tokens;
@@ -36,7 +58,7 @@ void printVetor(vector<string> tokens){
     cout << endl;
 }
 
-int executa(vector<string> linhaComando){
+int executa(vector<string> linhaComando, string inArquivo, string outArquivo){
     int pid = fork();
     int retorno;
     vector<char *> args;
@@ -45,83 +67,79 @@ int executa(vector<string> linhaComando){
             args.push_back(&linhaComando[i][0]);
         }
         args.push_back(nullptr);
-        return execvp(args[0], args.data());
+        cout << inArquivo << " " << outArquivo << endl;
+        if (inArquivo != ""){
+            cout << "Entrei aqui" << endl;
+            int fin = open(inArquivo.c_str(), O_RDONLY);
+            dup2(fin, 0);
+        }
+        if (outArquivo != ""){
+            int fout = open(outArquivo.c_str(), O_CREAT | O_WRONLY, S_IRWXU | S_IRWXG);
+            dup2(fout, 1);
+        }
+        execvp(args[0], args.data());
     }
     else{
         wait(NULL);
     }
 }
 
-void parse(vector<string> linhaComando, string inArquivo, string outArquivo){
+void trataBuiltin(vector<string> linhaComando, string inArquivo, string outArquivo){
     if (linhaComando[0] == "cd"){
         chdir(linhaComando[1].c_str());
     }
     else if (linhaComando[0] == "rm"){
-        if(!remove(linhaComando[1].c_str())){
+        if(remove(linhaComando[1].c_str()) == -1){
             cout << "Erro ao deletar arquivo" << endl;
         }
     }
     else if(linhaComando[0] == "historico"){
         if (linhaComando.size() != 1){
-            parse(historico[stoi(linhaComando[1])], "", "");
+            parse(historico[stoi(linhaComando[1])]);
         }
         else {
-            try{
-                for (int i = 0; i < historico.size(); i++){
-                    cout << i << " ";
-                    printVetor(historico[i]);
-                }
+            for (int i = 0; i < historico.size(); i++){
+                cout << i << " ";
+                printVetor(historico[i]);
             }
-            catch(exception e){}
         }
     }
     else
-        executa(linhaComando);
+        executa(linhaComando, inArquivo, outArquivo);
 }
 
-int main(){
-    Tabela tabela = Tabela(".unbshrc");
-    tabela.printTabela();
-    string entrada;
-    while (entrada != "exit"){
-        vector<string> linhaComando;
-        char cwd[100];
-        getcwd(cwd,sizeof(cwd));
-        cout << "UnBsh-user1-" << cwd<< ">";
-        getline(cin, entrada);
-        vector<string> tokens = split(entrada);
-        while (tokens.size()){
-            for (LinhaTabela linha : tabela.tabela){
-                if(tokens[0] == linha.alias){
-                    tokens[0] = linha.cmd;
-                }
+void parse(vector<string> tokens){
+    vector<string> linhaComando;
+    while (tokens.size()){
+        for (LinhaTabela linha : tabela.tabela){
+            if(tokens[0] == linha.alias){
+                tokens[0] = linha.cmd;
             }
-            historico.push_back(tokens);
-            if (historico.size() > 10){
-                historico.erase(historico.begin());
-            }
-            for (int i = 0; tokens.size() && tokens[i] != "|" && tokens[i] != "<" && tokens[i] != ">";){
-                linhaComando.push_back(tokens[i]);
-                tokens.erase(tokens.begin());
-            }
-            string inArquivo = "", outArquivo = "";
-            if (tokens[0] == "<"){
-                tokens.erase(tokens.begin());
-                inArquivo = tokens[0];
-                tokens.erase(tokens.begin());
-            }
-            if (tokens[0] == ">"){
-                tokens.erase(tokens.begin());
-                outArquivo = tokens[0];
-                tokens.erase(tokens.begin());
-            }
-            if (tokens[0] == "|" ){
-
-            }
-            parse(linhaComando, inArquivo, outArquivo);
         }
+        historico.push_back(tokens);
+        if (historico.size() > 10){
+            historico.erase(historico.begin());
+        }
+        for (int i = 0; tokens.size() && tokens[i] != "|" && tokens[i] != "<" && tokens[i] != ">";){
+            linhaComando.push_back(tokens[i]);
+            tokens.erase(tokens.begin());
+        }
+        string inArquivo = "", outArquivo = "";
+        if (tokens[0] == "<"){
+            tokens.erase(tokens.begin());
+            inArquivo = tokens[0];
+            tokens.erase(tokens.begin());
+        }
+        if (tokens[0] == ">"){
+            tokens.erase(tokens.begin());
+            outArquivo = tokens[0];
+            tokens.erase(tokens.begin());
+        }
+        if (tokens[0] == "|" ){
+
+        }
+        trataBuiltin(linhaComando, inArquivo, outArquivo);
     }
 }
-
 // char *args[] = {"ls" "-hla", NULL}
 // execv(args[0], args)
